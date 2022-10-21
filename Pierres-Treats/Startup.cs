e98dd42b92423f83c7a070c819c6dfa59cf21cfa
tgold83 <ted.jonathan.gold@gmail.com>
2022-10-21 09:56:36 -0700

    add new code to startup for password requirements

--- Pierres-Treats/Startup.cs
+++ Pierres-Treats/Startup.cs
@@ -35,11 +35,11 @@ namespace PierresTreats
 
       services.Configure<IdentityOptions>(options =>
       {
-        options.Password.RequireDigit = false;
-        options.Password.RequiredLength = 0;
-        options.Password.RequireLowercase = false;
+        options.Password.RequireDigit = true;
+        options.Password.RequiredLength = 6;
+        options.Password.RequireLowercase = true;
         options.Password.RequireNonAlphanumeric = false;
-        options.Password.RequireUppercase = false;
+        options.Password.RequireUppercase = true;
         options.Password.RequiredUniqueChars = 0;
       });
     }
